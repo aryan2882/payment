28c577e89eaa4571dd8f6a51e132ca233cb09167
Aryan RAJ <131025781+aryan2882@users.noreply.github.com>
2024-09-11 06:00:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,15 @@ const axios=require('axios');
 dotenv.config();
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:3001' })); // Adjust for frontend origin
+const corsOptions = {
+  origin: ['https://pg-self.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,  // Allows cookies/authentication headers
+  optionsSuccessStatus: 204  // Ensures successful response for OPTIONS requests
+};
+
+app.use(cors(corsOptions)); // Adjust for frontend origin
 app.use(express.json());
 
 // Initialize Razorpay
